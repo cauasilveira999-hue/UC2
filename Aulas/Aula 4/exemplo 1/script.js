fad013973d4f27d6100ae6caf2eb86825fd20e8b
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-09-23 19:45:35 -0300

    UC2

---
+++ Aulas/Aula 4/exemplo 1/script.js
@@ -0,0 +1,11 @@
+// /* exemplo 1 */
+
+/* Cauã */
+console.log ("entendi sim")
+
+// segundo ola mundo
+console.log ("segundo Ola Mundo")
+prompt ("Qual sua idade?")
+const nome = "Cauã"
+const idade = "16"
+console.log ("o nome é:",nome, "e sua idade é:", idade)
