28d3ded7bdb4c7cdb795026a7c52455e4eb8ba20
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-09-23 21:45:07 -0300

    adicionado aula5

--- Aulas/Aula 4/exemplo 1/Exercicios/script.js
+++ Aulas/Aula 4/exemplo 1/Exercicios/script.js
@@ -2,4 +2,5 @@ const nome = "Cauã"
 const sobrenome = "Siveira"
 const idade = 16
 const estudante = true
-console.log ("Qual é seu nome?:", nome, "Qual seu sobrenome?:", sobrenome, "Qual a sua idade?:", idade, "Voce é estudante?:", estudante)
+typeof 
+console.log ("Qual é seu nome?:", nome, "Qual seu sobrenome?:", sobrenome, "Qual a sua idade?:", idade, "Voce é estudante?:", estudante, )
---
+++ Aulas/Aula5/exemplo/exercicios/script.js
@@ -0,0 +1,11 @@
+
+const nome = prompt("Qual seu nome?")
+console.log(nome)
+const idade = prompt("Qual sua idade?")
+console.log(idade)
+
+console.log(typeof(nome))
+console.log(typeof(idade))
+
+const idadeNumber = Number(idade)
+console.log(typeof(idadeNumber))
