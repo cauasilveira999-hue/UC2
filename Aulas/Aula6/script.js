b72b099e264cc95577bd17a9beab9bdf2135c5e8
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-09-24 21:50:24 -0300

    adicionado Aula6

---
+++ Aulas/Aula6/script.js
@@ -0,0 +1,52 @@
+/* let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+let declara que a variavel pode ser modificada no meio do codigo, console.log imprime as informações no terminal, 
+o codigo ele está guardando dois numeros nas variaveis a & b, mas após dar console.log, ele muda o numero dentro
+da declaração "let = b" para 5, assim adicionando mais uma informação a variavel "b" logo após ele imprime as informações com console.log, as informações vão ser,
+impressas desta forma:
+
+10
+10 5 
+*/
+
+
+/*let a = 10
+let b = 20
+
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+/*no codigo tem duas duas variaveis com a declaração "let" ou seja, pode ser modificada no meio do codigo,
+as variaveis são "let a = 10" e "let = 20" e depois ele substitui 3 informações das variaveis, e depois
+ele imprime as informações no terminal, assim:
+
+10 
+10 
+10
+*/
+
+/*
+let horasTrabalho = prompt("Quantas horas você trabalha por dia?")
+let salarioDia = prompt("Quanto você recebe por dia?")
+console.log("Você recebe",horasTrabalho,"/",salarioDia,"por hora")
+
+/*o codigo utiliza da declaração "let" e do comando "prompt" que faz uma pergunta para o
+usuario, assim o usuario pode colocar quantas horas ele trabalha e quanto recebe por dia, assim as informações
+são salvas e expostas no console a partir do comando "console.log"*/
+
+
+let nome
+let idade 
+
+/* esse tipo foi impresso para que o usuario possa colocar as suas informações*/
+
+nome = prompt =("Qual seu nome?")
+idade = prompt("Qual sua idade?")
