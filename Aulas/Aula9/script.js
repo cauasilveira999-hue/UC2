892b682039b53043c2aec84e151417bb4b938c80
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-10-03 21:38:43 -0300

    Adicionado Aula9

--- Aulas/Aula9/script.js
+++ Aulas/Aula9/script.js
@@ -51,18 +51,100 @@ console.log(idade2050 = ano2050 - anoNascimento)
 
 console.log("Qual seu nome?:", nome, "Qual sua idade?:", idade, "Você é maior de idade?:", maiorDeIdade, "Idade em 2050:", idade2050)
 */
-
+/* 
 const bool1 = true
 const bool2 = false
 const bool3 = !bool2
 
 let resultado = bool1 && bool2
-console.log("a. ", resultado)
+console.log("a. ", resultado) // false
 
 resultado = bool1 && bool2 && bool3
-console.log("b. ", resultado)
+console.log("b. ", resultado) // false
 
 resultado = !resultado && (bool1 || bool2)
-console.log("c. ", resultado)
+console.log("c. ", resultado) // true
 
 console.log("d. ", typeof resultado)
+
+
+a. false
+b. false
+c. true
+d. boolean
+*/
+/*
+let primeiroNumero = prompt("Digite um numero")
+let segundoNumero = prompt("Digite outro numero")
+
+primeiroNumero = Number(primeiroNumero)
+segundoNumero = Number(segundoNumero)
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+// as variaveis não foram convertidas para "Number"
+
+
+let idade = prompt("Qual sua idade?")
+let idadeAmigo = prompt("Qual a idade do seu amigo?")
+let diferença
+let resultado = idade > idadeAmigo
+
+console.log("Sua idade é maior que a do seu amigo?:", resultado)
+
+diferença = idade % idadeAmigo
+
+console.log("A diferença de idade de vocês é:", diferença)
+
+
+
+let numeroPar = prompt("Insira um numero par")
+let resultado
+resultado = (numeroPar / 2) 
+console.log("O resto da divisão é:", resultado)
+
+// notei o padrão de sempre serem numeros sem virgula
+// se utilizar um numero impar o console exibe um resultado com virgula
+
+
+let idade = prompt("Qual sua idade em anos?")
+let meses // * 12
+let dias // * 365
+let horas // * 24
+
+meses = idade * 12
+dias = idade * 365
+horas = idade * 8766
+
+console.log("Sua idade é:", idade, "Sua idade em meses é:", meses, "Sua idade em dias é:", dias, "Sua idade em horas é:", horas)
+
+
+let primeiroNumero = prompt("Diga um numero")
+let segundoNumero = prompt("Diga outro")
+let resultado
+let resultado2
+let resultado3
+let resultado4
+
+resultado = primeiroNumero > segundoNumero
+resultado2 = primeiroNumero === segundoNumero
+resultado3 = primeiroNumero <= segundoNumero
+resultado4 = segundoNumero >= primeiroNumero
+
+console.log("O primeiro numero é maior que o segundo?:", resultado, "O primeiro numero é igual ao segundo?:", resultado2, "O primeiro numero é divisivel pelo segundo?:", resultado3, "O segundo numero é divisivel pelo primeiro?:", resultado4)
+
+
+Kelvin = (77 - 32)*(5/9) + 273.15
+console.log(Kelvin,"K")
+
+fahrenheit = (80)*(9/5) + 32
+console.log(fahrenheit,"°F")
+
+celsiusKelvin = (30)*(9/5) + 32
+celsiusFahreinheit = (30 - 32)*(5/9) + 273.15
+
+console.log(celsiusKelvin,"K", celsiusFahreinheit, "°F")
+*/
+
