3b8ed30d733bf5a4c23f853e89b31481625ed7b9
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-10-07 21:47:17 -0300

    Adicionado Aula11

--- Aulas/Aula10/script.js
+++ Aulas/Aula10/script.js
@@ -60,10 +60,52 @@ console.log(frase)
 
 const frase = "Hoje comi cenoura, adoro cenoura"
 const novaFrase = frase.replaceAll("cenoura","batata")
-console.log(novaFrase)*/
+console.log(novaFrase)
 
 let frase = prompt("Escreva uma frase")
 console.log(frase.toUpperCase())
 const novaFrase = frase.replaceAll("o","i")
 console.log(novaFrase)
 console.log(frase.length)
+
+const listaDeCompras = ["abacate", "banana", "tomate"]
+
+console.log(listaDeCompras[1])
+
+
+const racasCachorro = ["Pinscher", "Poodle", "Golden", "Pastor Alemão", "Husky"]
+let numeroUsuario = prompt("Insira um numero de 0 a 4")
+console.log(racasCachorro[numeroUsuario])
+
+const pokemons = ["Raychu", "Arceus", "Combusken"]
+console.log(pokemons.length)
+
+const seriesBoas = ["The Vampire Diaries", "Demon Slayer", "Sobrenatural"]
+console.log(seriesBoas.includes("The Vampire Diaries"))
+
+const numeros  = [1, 2, 3]
+numeros.push(4)
+console.log(numeros)
+
+numeros.push(5, 6, 7)
+console.log(numeros)
+
+
+const peixes = ["Palhaço", "Mandarim", "Esturjão"]
+peixes.pop()
+console.log(peixes)
+
+
+const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+letras.splice(3,2)
+console.log(letras)*/
+
+const numeros = [1, 2, 3, 4, 5, 6]
+console.log(numeros.length)
+numeros.push(7)
+console.log(numeros)
+
+numeros.splice(3,2)
+console.log(numeros)
+
+console.log(numeros.length)
