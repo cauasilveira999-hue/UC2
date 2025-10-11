5f638d25c9e9836f97714f713cb0011ada0007f4
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-10-10 21:48:26 -0300

    Adicionado DiaMaluco

---
+++ Aulas/DiaMaluco/script.js
@@ -0,0 +1,28 @@
+/*let numeroAcessorio = prompt("Quantos acessorios você deseja ter? 1 a 10")
+let numeroCores = prompt("Quantas cores vai ter? 1 a 10")
+let fatorBrilho = prompt("Digite o quanto de brilho. 1 a 10")
+
+let pontuacao = (numeroAcessorio*5) + (numeroCores*3) + (fatorBrilho*10)
+let metaPontuacao = 80
+
+let resultado = (pontuacao >= metaPontuacao)
+
+if (resultado === true){console.log("Aprovado")}
+else console.log(" Falta ", metaPontuacao - pontuacao, "Pontos")
+*/
+
+let descricao = prompt("Descreva seu look")
+let corPrincipal = prompt("Qual a cor de destaque do look")
+
+let resultado = descricao.length
+
+let temTamanhoAdequado = 20
+let minuscula
+
+console.log(resultado >= temTamanhoAdequado)
+
+minuscula = descricao.toLowerCase
+
+console.log(descricao.indexOf(" listras ", " brilho "))
+
+
