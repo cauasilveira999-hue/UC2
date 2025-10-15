adcfd8c13dc4efa88dcaaec4538d0a40b0e37099
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-10-14 21:55:04 -0300

    Adicionado Aula12

---
+++ Aulas/Aula12/script.js
@@ -0,0 +1,104 @@
+/*let condicao1 = true
+let condicao2 = false
+let condicao3 = true
+if(condicao1) {
+    /* como o valor da condição é true
+    o codigo no bloco é executado*/
+
+
+
+
+    /* como a condição é false
+    o codigo não é executado
+    console.log("Entrei no if 2")
+}
+
+
+let num1 = prompt("Digite um numero")
+let num2 = prompt("Digite outro numero")
+
+if(num1===num2){
+    console.log("Os numeros são iguais")
+} else {
+
+console.log("Os numeros são diferentes")
+}
+
+
+if(condicao1){
+    console.log("Entrou no if 1")
+}
+
+else if(condicao2){
+    console.log("Entrou no else 1")
+}
+
+else if(condicao3){
+    console.log("Entrou no if 2")
+}
+
+else {
+console.log("Entrou no else")
+}
+
+
+let num1 = prompt("Digite um numero")
+let num2 = prompt("Digite outro numero")
+
+if(num1>num2){
+    console.log("O primeiro numero é maior que o segundo")
+}
+
+else if(num1<num2){
+    console.log("O primeiro numero é menor que o segundo")
+}
+
+else if(num1===num2){
+    console.log("O primeiro numero é igual ao segundo")
+}
+
+let paisOrigem = prompt("Digite seu pais de origem")
+
+switch(paisOrigem){
+    case 'Brasil':
+        console.log("Nacionalidade Brasileiro")
+    break
+
+ case 'EUA':
+    console.log("Nacionalidade Norte Americano")
+    break
+
+case 'Espanha':
+    console.log("Nacionalidade Espanhola")
+    break
+
+case 'India':
+    console.log("Nacionalidade Indiana")
+    break
+
+default:
+    console.log("Nacionalidade não definida")
+    break
+}*/
+
+
+let pokemon = prompt("Digite um pokemon inicial")
+
+
+switch(pokemon){
+    case 'Bulbasaur':
+        console.log("Tipo: planta e veneno")
+        break
+
+    case 'Charmander':
+        console.log("Tipo: fogo")
+        break
+
+    case 'Squirtle':
+        console.log("Tipo: agua")
+        break
+
+    default:
+        console.log("Nenhum pokemon escolhido")
+        break
+}
