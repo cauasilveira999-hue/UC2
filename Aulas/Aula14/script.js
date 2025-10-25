0fa222874224f2d779098ef73ba0d76b2346adc2
CAUA SILVEIRA MORALES <60277490081@senacrs.edu.br>
2025-10-24 21:50:01 -0300

    Aula14

---
+++ Aulas/Aula14/script.js
@@ -0,0 +1,47 @@
+/*let i = 0
+
+while(i <= 9){ // enquanto a condição (que no caso é "i") for menor que nove ele executa o console.log e incrementa o interador
+    console.log("Loop;", i)
+    i++
+}
+
+console.log('Chegou ao final do codigo!')
+
+
+let estomago = 0
+
+while(estomago < 100){
+    console.log("Quero comer mais coxinhas!:", estomago)
+    estomago += 10 
+}
+let soma = 0
+let numero 
+ while(numero!== 0){
+  numero = Number(prompt("Digite um numero"))
+    soma += numero
+ }
+ console.log("O resultado é:", soma
+
+ let c = 1
+ 
+ do{
+console.log("O valor de C é:", c)
+c++
+ }while(c <= 6))
+
+ for(let i=1; i<9; i++){
+    console.log('O valor de I é:', i)
+ }
+
+ const meuArray = [10, 4, 7, 8, 100, 77]
+ for(let i=0; i<6; i++){
+    console.log(meuArray[i])
+ }*/
+
+ let maiorNumero = 0
+ const meuArray = [11, 15, 18, 14, 12, 13]
+ for(let i=0; i<6; i++){
+    if(maiorNumero > meuArray[i])
+    maiorNumero = meuArray[i]
+    console.log("O maior numero é:", maiorNumero)
+ }
