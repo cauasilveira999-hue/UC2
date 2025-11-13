6a95ae1c57bc61e51ae4c0bc3f9a6ac9a479ed4e
CAUA SILVEIRA MORALES <60277490081@senacrs.edu.br>
2025-11-12 21:41:48 -0300

    Adicionado Aula18

--- Aulas/Aula18/script.js
+++ Aulas/Aula18/script.js
@@ -7,13 +7,12 @@
     }
 }
 
-console.log("O nome da pessoa é", namorada.informacoes.nome, "e suas comidas preferidas são", namorada.comidasFav = namorada.comidasFav.join(", "), "Seu melhor amigo se chama", namorada.informacoes.melhorAmigo, "e tem", namorada.informacoes.idade, "anos.")*/
+console.log("O nome da pessoa é", namorada.informacoes.nome, "e suas comidas preferidas são", namorada.comidasFav = namorada.comidasFav.join(", "), "Seu melhor amigo se chama", namorada.informacoes.melhorAmigo, "e tem", namorada.informacoes.idade, "anos.")
 
 const arrayPersonagens = [
-    { nome: "Naruto", idade: 16, estilo: "Taijutsu", jutsus: "Vortex de Água, Técnica Harém, Bomba Bijuu" },
+    { nome: "Naruto", idade: 16, estilo: "Ninjutsu", jutsus: "Vortex de Água, Técnica Harém, Bomba Bijuu" },
     { nome: "Hinata", idade: 16, estilo: "Taijutsu", jutsus: "Byakungan, Palma Inferior, Punho Suave" },
     { nome: "Sakura", idade: 16, estilo: "Taijutsu", jutsus: "Atemi, Queda do Tronco, Técnica de Transformação" }
-
 
 ]
 
@@ -25,4 +24,62 @@ function adicionarPersonagem() {
 }
 adicionarPersonagem()
 
-console.log(arrayPersonagens)
+function buscarPersonagem() {
+    let personagem = prompt("Escolha um personagem")
+    return personagem
+}
+buscarPersonagem()
+
+if (arrayPersonagens.includes(personagem)){
+    console.log(arrayPersonagens[0].nome)
+}
+
+
+
+const filme = {
+    nome: "Auto da Compadecida",
+    ano: 2000,
+    elenco:[
+        "Matheus Nachtergaele", "Selton Mello", "Denise Fraga", "Virginia Cavendish"
+    ],
+    transmissoesHoje:[
+        {canal: "Telecine", horario: "21"},
+        {canal: "Canal Brasil", horario: "19"},
+        {canal: "Globo", horario: "14"}
+    ]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+/* vai ser impresso:
+
+Matheus Nachtergaele
+Virginia Cavendish
+Object
+canal: "Canal Globo"
+horario: "14"*/
+
+
+const cachorro = {
+    nome: "Juca",
+    idade: 3,
+    raca: "SRD"
+}
+
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+
+/* será impresso é:
+nome: Juca
+idade: 3
+raca: SRD
+nome: Juba
+nome: Goto
+*/
