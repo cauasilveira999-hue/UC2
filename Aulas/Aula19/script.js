474f316637eea11ccfc9d1008e70b55e9871c486
CAUA SILVEIRA MORALES <60277490081@senacrs.edu.br>
2025-11-17 21:48:24 -0300

    Aula19

---
+++ Aulas/Aula19/script.js
@@ -0,0 +1,35 @@
+/*let jogoDaVelha = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+jogoDaVelha[1][2] = 1
+
+console.log(jogoDaVelha)
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+let soma = 0
+for(let i = 0; i < matriz.length; i++){
+    for(let j = 0; j < matriz[i].length; j++)
+    soma += matriz[i][j]
+}
+ console.log("a soma dos numeros da matriz é:", soma)
+*/
+
+ let matriz = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+let soma = 0
+for(let i = 0; i < matriz.length; i++){
+    for(let j = 0; j < matriz[i].length; j++)
+    soma += matriz[i][j]
+}
+ console.log("a soma dos numeros da matriz é:", soma)
